6f0d8ed0ecf41d68944ee5e0f55ab32f8288a7f4
divyanshubose2408-a11y <divyanshubose2408@gmail.com>
2026-08-18 21:04:45 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,6 +1,4 @@
 // Make a calculator program using simple class 
-import java.util.Scanner;
-
 class Calculator {
     int add(int a, int b) {
         return a + b;
@@ -19,27 +17,20 @@ class Calculator {
             System.out.println("Error: Division by zero is not allowed.");
             return 0;
         }
-        return  a / b;
+        return a / b;
     }
 }
 
-public class Lucky1 {
+public class DB {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
+        int num1 = 20;
+        int num2 = 5;
         
-        System.out.print("Pehla number enter karein: ");
-        int num1 = scanner.nextInt();
-        
-        System.out.print("Dusra number enter karein: ");
-        int num2 = scanner.nextInt();
-        
-        System.out.println("\nInputs: " + num1 + " and " + num2);
+        System.out.println("Inputs: " + num1 + " and " + num2);
         System.out.println("Addition: " + calc.add(num1, num2));
         System.out.println("Subtraction: " + calc.subtract(num1, num2));
         System.out.println("Multiplication: " + calc.multiply(num1, num2));
         System.out.println("Division: " + calc.divide(num1, num2));
-        
-        scanner.close();
     }
 }
